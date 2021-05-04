1f6c985cd08bc36f023eac26ddc6bc9571fb9e43
DeepGajera91 <deepgajera91@gmail.com>
2021-05-05 02:19:58 +0530

    bug fixing

--- Routes/user.js
+++ Routes/user.js
@@ -409,6 +409,7 @@ router.post("/resetpassword",[
         min:8
     }),
 ],async(req,res)=>{
+    console.log(req.body);
     const errors = validationResult(req);
         if(!errors.isEmpty()){
             const response = {
